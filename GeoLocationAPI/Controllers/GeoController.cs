679edca15210c28d868be1b849ed79756aff9554
Omar Salah <63765054+Omar-hk@users.noreply.github.com>
2022-09-27 18:25:48 +0200

    Add files via upload

---
+++ GeoLocationAPI/Controllers/GeoController.cs
@@ -0,0 +1,31 @@
+﻿using GeoLocationAPI.Models;
+using MaxMind.GeoIP2;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoLocationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeoController : ControllerBase
+    {
+        [HttpGet(template:"[action]/{ipAddress}")]
+         public IActionResult GetCountry(string ipAdress)
+        {
+            using (var reader = new DatabaseReader(@"C:\Users\Shafntry\source\repos\db\Geo.mmdb"))
+            {
+                var response = reader.Country(ipAdress);
+
+                var geoLocation = new GeoClass();
+                geoLocation.CountryName = response.Country.Name;
+                geoLocation.CountryIsoCode = response.Country.IsoCode;
+                geoLocation.IsInEuropeanUnion = response.Country.IsInEuropeanUnion;
+
+                return StatusCode(StatusCodes.Status200OK, geoLocation);
+                Console.WriteLine(response.Country.IsoCode);
+            }
+        }
+
+
+    }
+}
---
+++ GeoLocationAPI/Models/GeoClass.cs
@@ -0,0 +1,11 @@
+﻿namespace GeoLocationAPI.Models
+{
+    public class GeoClass
+    {
+        public string CountryName { get; set; }
+
+        public string CountryIsoCode { get; set; }
+
+        public bool IsInEuropeanUnion { get; set; }
+    }
+}
